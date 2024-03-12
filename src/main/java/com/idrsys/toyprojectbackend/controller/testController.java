b5b48d8ae460b53ac4ae7c6ca57a91a6173cea88
gimjuncheol <gimjuncheol839@idrsystems.co.kr>
2024-03-12 13:32:39 +0900

    프론트 연동 및 테스트

---
+++ src/main/java/com/idrsys/toyprojectbackend/controller/testController.java
@@ -0,0 +1,13 @@
+package com.idrsys.toyprojectbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트입니다.";
+    }
+}
